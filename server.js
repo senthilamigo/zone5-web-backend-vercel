7fe3686f5b2b9e525f6ffc6c63812f1efcfa754f
Senthilkumar Parthasarathy <senthilkumar.parthasarathy@cognizant.com>
2026-02-22 21:47:12 +0530

    fix entry point

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('./src/config'); // loads dotenv
 const express = require('express'); // required for Vercel entrypoint detection
-const app = require('./src/app');
+const app = require('./src/modules/app');
 
 // Export for Vercel serverless functions
 module.exports = app;
